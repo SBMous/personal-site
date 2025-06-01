688fd5e94c46635ed6d1e1006492cb7e9e47d32f
George Yarnley <george@yarnley.com>
2025-06-02 00:57:56 +0100

    Fix build

--- src/model/data/projects.ts
+++ src/model/data/projects.ts
@@ -64,7 +64,7 @@ export const projects: Project[] = [
     links: [
       {
         icon: 'ri-github-line',
-        target: '',
+        target: 'https://github.com/SBMous/personal-site',
       },
     ],
     content:
